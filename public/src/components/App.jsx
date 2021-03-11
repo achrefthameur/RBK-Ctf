e447ad4fa3500725693b5041c29b657d4ef7e900
achrefthameur <benthameur@mail.com>
2021-03-11 22:19:02 +0100

    added react-particles For background

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Particles from 'react-particles-js';
+import particlesConfig from '../config/particlesConfig.js';
 
 import Navbar from './Navbar.jsx';
 import Login from './Login.jsx';
 import SignUp from './SignUp.jsx';
 import ScoarBoard from './ScoardBoard.jsx';
 import Challanges from './Challanges.jsx'
+
 class App extends React.Component {
     constructor(props) {
       super(props);
@@ -17,6 +20,9 @@ class App extends React.Component {
     render () {
       return (    
       <div >
+            <div style={{ position: 'absolute'}}>
+              <Particles height="100vh" width="100vw" params={particlesConfig} />
+            </div>
            <Navbar />          
       </div>)
     }
