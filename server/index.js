8a0b495bf10129d68a4a7d6169e1bf6759f74f92
achrefthameur <benthameur@mail.com>
2021-03-11 23:20:23 +0100

    cookieParser middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
 var path = require('path')
 var routes = require('./routes')
+
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cookieParser())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/../public/www'));
