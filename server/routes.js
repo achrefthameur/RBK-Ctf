1a86a21b487ae5dd5ee27c00f515d51b17123ef6
achrefthameur <benthameur@mail.com>
2021-03-11 23:40:39 +0100

    session router

--- server/routes.js
+++ server/routes.js
@@ -2,8 +2,11 @@ const controller = require('./controllers');
 const middleware = require('./middleware');
 const router  = require('express').Router();
 
+
 router.post('/api/login',controller.Login.login);
 router.post('/api/register',controller.teams.CreateTeam);
+
+router.get('/api/session',middleware.authentification.VerifySession)
 
 router.get('/api/scoardBoard',controller.teams.GetTop);
 
